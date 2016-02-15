4e64ef597de68d8e93e114dec907f1fc9a577080
Hoxie Ackerman <hoxiea@gmail.com>
2016-02-15 12:37:22 -0500

    Implement quite a bit of Q-Learning, along with notes about how to proceed

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -3,22 +3,57 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 from collections import namedtuple
+from itertools import product
 
 State = namedtuple('State', 'light oncoming left right next_waypoint')
 
+# TODO: can we get away with a simpler state? I'm not sure that 'right' provides any useful information
+# If the light is red:
+# - And there's nobody to your left going forward, then you can take a right on red
+# - Don't need to worry about oncoming, since light is red for them too, and no left thru red light
+# - Don't need to worry about right, since nothing they do can interfere with you
+# If the light is green:
+# - You can go forward or right with the right of way and no problems
+# - You can go left if there's nobody oncoming going forward
+# In both cases, right is irrelevant
+
 class LearningAgent(Agent):
-    """An agent that learns to drive in the smartcab world."""
+    """
+    An agent that learns to drive in the smartcab world.
+
+    TODO: discuss the main learning data structure, q_map
+    """
 
     def __init__(self, env):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
-        self.color = 'red'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        # Simulation stuff
+        super(LearningAgent, self).__init__(env)
+        self.color = 'red'
+        self.planner = RoutePlanner(self.env, self)
+
+        # Learning stuff
         self.current_state = None
-        # TODO: Initialize any additional variables here
+        self.alpha = 0.5   # learning rate
+        self.gamma = 0.8   # discount rate
+        self.q_map = dict()
+
+        valid_light = Environment.valid_inputs['light']
+        valid_oncoming = Environment.valid_inputs['oncoming']
+        valid_left = Environment.valid_inputs['left']
+        valid_right = Environment.valid_inputs['right']
+        valid_waypoints = Environment.valid_actions
+        for state in product(valid_light, valid_oncoming, valid_left, valid_right, valid_waypoints):
+            self.q_map[state] = dict()
+            for action in Environment.valid_actions:
+                self.q_map[state][action] = random.random()
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
+        self.current_state = None
+        # I don't think we want to reset q_map here... all the agents get reset
+        # after each trial, so if we're going to remember what we learn, we
+        # shouldn't clear out q_map
+        # self.q_map = dict()
+        # TODO: reset any other variables here
 
     def update(self, t, debug=True):
         # Gather inputs
@@ -36,13 +71,27 @@ class LearningAgent(Agent):
         if debug:
             print self.current_state
 
-        # Current policy: Select a random action
-        action = random.choice(Environment.valid_actions)
+        # Select the action for the current state with the largest Q value
+        action_values = self.q_map[self.current_state]
+        action = max(action_values, key=action_values.get)
+        assert action in Environment.valid_actions
+        if debug:
+            print "Action choices:"
+            print action_values
+            print "Selected action: {}".format(action)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
-        # TODO: Learn policy based on state, action, reward
+        # Calling act causes location to change if a valid move was made
+        # But will other agents have moved, giving us updated intersection information?
+        # TODO: figure this out and you're done
+        next_state = 0
+
+        # Learn policy based on state, action, reward via Q-Learning update
+        previous_value = self.q_map[state][action]
+        future_value = reward + self.gamma * max(self.q_map[next_state].itervalues())
+        self.q_map[state][action] = (1 - self.alpha) * previous_value + (self.alpha * future_value)
 
         if debug:
             print "LA.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)
