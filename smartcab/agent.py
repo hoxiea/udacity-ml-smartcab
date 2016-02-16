a6209b0e097701fad35a02c35788d161edad0849
Hoxie Ackerman <hoxiea@gmail.com>
2016-02-16 11:49:25 -0500

    Simplify state to only light and next_waypoint; implement Q-Learning with initializations that favor heading to next_waypoint; learns to reach destination in <10 trials

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -5,7 +5,8 @@ from simulator import Simulator
 from collections import namedtuple
 from itertools import product
 
-State = namedtuple('State', 'light oncoming left right next_waypoint')
+# State = namedtuple('State', 'light oncoming left right next_waypoint')
+State = namedtuple('State', 'light next_waypoint')
 
 # TODO: can we get away with a simpler state? I'm not sure that 'right' provides any useful information
 # If the light is red:
@@ -36,15 +37,20 @@ class LearningAgent(Agent):
         self.gamma = 0.8   # discount rate
         self.q_map = dict()
 
-        valid_light = Environment.valid_inputs['light']
-        valid_oncoming = Environment.valid_inputs['oncoming']
-        valid_left = Environment.valid_inputs['left']
-        valid_right = Environment.valid_inputs['right']
+        valid_light = ['green', 'red']   # as returned by Environment.sense
+        # valid_oncoming = Environment.valid_inputs['oncoming']
+        # valid_left = Environment.valid_inputs['left']
+        # valid_right = Environment.valid_inputs['right']
         valid_waypoints = Environment.valid_actions
-        for state in product(valid_light, valid_oncoming, valid_left, valid_right, valid_waypoints):
+        for state in product(valid_light, valid_waypoints):
             self.q_map[state] = dict()
+            state_light, state_waypoint = state
             for action in Environment.valid_actions:
-                self.q_map[state][action] = random.random()
+                if state_waypoint == action:
+                    self.q_map[state][action] = 1 + random.random()
+                else:
+                    self.q_map[state][action] = random.random()
+        print self.q_map
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -52,7 +58,6 @@ class LearningAgent(Agent):
         # I don't think we want to reset q_map here... all the agents get reset
         # after each trial, so if we're going to remember what we learn, we
         # shouldn't clear out q_map
-        # self.q_map = dict()
         # TODO: reset any other variables here
 
     def update(self, t, debug=True):
@@ -63,18 +68,17 @@ class LearningAgent(Agent):
 
         # Update state
         light = inputs['light']
-        oncoming = inputs['oncoming']
-        left = inputs['left']
-        right = inputs['right']
+        # oncoming = inputs['oncoming']
+        # left = inputs['left']
+        # right = inputs['right']
         next_waypoint = self.next_waypoint
-        self.current_state = State(light, oncoming, left, right, next_waypoint)
+        self.current_state = State(light, next_waypoint)
         if debug:
             print self.current_state
 
         # Select the action for the current state with the largest Q value
         action_values = self.q_map[self.current_state]
         action = max(action_values, key=action_values.get)
-        assert action in Environment.valid_actions
         if debug:
             print "Action choices:"
             print action_values
@@ -85,15 +89,21 @@ class LearningAgent(Agent):
 
         # Calling act causes location to change if a valid move was made
         # But will other agents have moved, giving us updated intersection information?
-        # TODO: figure this out and you're done
-        next_state = 0
+        new_status = self.env.sense(self)
+        new_light = new_status['light']
+        new_waypoint = self.planner.next_waypoint()
+        new_state = State(new_light, new_waypoint)
 
         # Learn policy based on state, action, reward via Q-Learning update
-        previous_value = self.q_map[state][action]
-        future_value = reward + self.gamma * max(self.q_map[next_state].itervalues())
-        self.q_map[state][action] = (1 - self.alpha) * previous_value + (self.alpha * future_value)
+        previous_value = self.q_map[self.current_state][action]
+        future_value = reward + self.gamma * max(self.q_map[new_state].itervalues())
+        updated_q_value = (1 - self.alpha) * previous_value + (self.alpha * future_value)
+        self.q_map[self.current_state][action] = updated_q_value
 
         if debug:
+            print "Old value for ({}, {}, {}): {}".format(light, next_waypoint, action, previous_value)
+            print "Reward received: {}".format(reward)
+            print "Updated value for ({}, {}, {}): {}".format(light, next_waypoint, action, updated_q_value)
             print "LA.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)
 
 
@@ -103,11 +113,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=10)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=.3)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
