328c90b5446aec42cc15c7edae98d3ed06f0f789
Hoxie Ackerman <hoxiea@gmail.com>
2016-03-01 13:31:46 -0500

    Refactor training/evaluation logic into hierarchical functions; make plotting a method on TrainEvalPerformance

--- smartcab/agentperformance.py
+++ smartcab/agentperformance.py
@@ -2,47 +2,98 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from collections import namedtuple
+from itertools import product
 from simulator import initialize_simulator_environment
 from strategies import *
 
-class PerformanceTracker(object):
-    def __init__(self, performances, agent_info):
-        self.performances = performances
+class TrainEvalPerformance(object):
+    """
+    A sequence of PrimaryAgentPerformances, along with helper methods for
+    analyzing those performances.
+    """
+    def __init__(self, training_perfs, eval_perfs, agent_info):
+        self.training_perfs = training_perfs
+        self.eval_perfs = eval_perfs
         self.agent_info = agent_info
 
-    @property
-    def prop_reached_destination(self):
-        return np.mean([p.reached_dest for p in self.performances])
+    def plot(self):
+        all_paps = self.training_perfs + self.eval_perfs
+        total_num_trials = len(all_paps)
+
+        reached, stalled = separate_performances(all_paps)
+
+        reached_x = [trial for trial, pap in reached]
+        reached_y_positive = [pap.positive_reward for _, pap in reached]
+        reached_y_negative = [pap.negative_reward for _, pap in reached]
+
+        stalled_x = [trial for trial, pap in stalled]
+        stalled_y_positive = [pap.positive_reward for _, pap in stalled]
+        stalled_y_negative = [pap.negative_reward for _, pap in stalled]
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        xmin = -3
+        xmax = total_num_trials + 3
+        ymin = min(reached_y_negative + stalled_y_negative) - 3
+        ymax = max(reached_y_positive + stalled_y_positive) + 5
+        xhalfway = len(self.training_perfs) + 1.5
+
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)
+
+        title = "Alpha: {}, Gamma: {}, Strategy: {}"
+        title = title.format(self.agent_info['alpha'], self.agent_info['gamma'],
+            self.agent_info['strategy'])
+        ax.set_title(title)
+
+        # Create the scatter plots
+        ax.scatter(reached_x, reached_y_positive, c='green', marker='o')
+        ax.scatter(stalled_x, stalled_y_positive, c='green', marker='x')
+        ax.scatter(reached_x, reached_y_negative, c='red', marker='o')
+        ax.scatter(stalled_x, stalled_y_negative, c='red', marker='x')
+
+        # Add lines
+        ax.plot((0, total_num_trials), (0, 0), "k--")  # reward == 0
+        ax.plot((xhalfway,)*2, (ymin, ymax), "k--")  # separate train/eval
+
+        # Add textual information
+        ax.set_xlabel('Trial #')
+        ax.set_ylabel('Reward')
+        plt.show()
+
 
 # Performance evaluation
-def evaluate_performance():
+def parameter_gridsearch(num_train=100, num_eval=100):
     """
     Run training trials for each of many learning parameter/strategy
     combinations, then run evaluation trials to see how well we learned.
+
+    Yields a sequence of TrainEvalPerformances.
     """
-    alpha_values = [.2, .4, .6, .8]
-    gamma_values = [.2, .4, .6, .8]
-    strategies = [weighted_q_average, decay_logarithmic, decay_linear, decay_exponential]
+    alpha_values = (.2, .4, .6, .8)
+    gamma_values = (.2, .4, .6, .8)
+    strategies = (explorer, weighted_q_average, decay_logarithmic, decay_linear, decay_exponential)
 
-    num_training_trials = 100
-    num_evaluation_trials = 100
-    all_results = dict()
-
-    for param_values in product(alpha_values, gamma_values, strategies):
-        evaluate_performance_helper(*param_values)
-
-    return all_results
+    for a, g, s in product(alpha_values, gamma_values, strategies):
+        agent_params = {'alpha': a, 'gamma': g, 'strategy': s}
+        yield train_evaluate(agent_params, num_train, num_eval)
 
 
-def evaluate_performance_helper(alpha, gamma, strategy, q_boost=0, \
-        num_training_trials=100, num_evaluation_trials=100, num_repetitions=10):
-    agent_params = {'alpha': alpha, 'gamma': gamma, 'q_boost': q_boost, 'strategy': strategy}
-    for _ in xrange(num_repetitions):
-        sim, e = initialize_simulator_environment(agent_params)
-        training_performances = sim.run(num_training_trials)
-        e.primary_agent.learning = False
-        evaluation_performances = sim.run(num_evaluation_trials)
-    return training_performances, evaluation_performances
+def train_evaluate(agent_params, num_train=100, num_eval=100):
+    """
+    Run num_train training trials and num_eval evaluation trials for a
+    LearningAgent initialized with a single set of agent_params.
+
+    Returns a TrainEvalPerformance that captures performances and agent info.
+    """
+    sim, e = initialize_simulator_environment(agent_params)
+
+    training_perfs = sim.run(num_train)
+    e.primary_agent.stop_learning()
+    evaluation_perfs = sim.run(num_eval)
+
+    agent_info = e.primary_agent.agent_info
+    return TrainEvalPerformance(training_perfs, evaluation_perfs, agent_info)
 
 
 def separate_performances(paps):
@@ -63,47 +114,49 @@ def separate_performances(paps):
     assert len(reached) + len(stalled) == len(paps)
     return reached, stalled
 
-def plot_performances(training_paps, evaluation_paps):
-    all_paps = training_paps + evaluation_paps
-    total_num_trials = len(all_paps)
 
-    reached, stalled = separate_performances(all_paps)
-
-    reached_x = [trial for trial, pap in reached]
-    reached_y_positive = [pap.positive_reward for _, pap in reached]
-    reached_y_negative = [pap.negative_reward for _, pap in reached]
-
-    stalled_x = [trial for trial, pap in stalled]
-    stalled_y_positive = [pap.positive_reward for _, pap in stalled]
-    stalled_y_negative = [pap.negative_reward for _, pap in stalled]
-
-
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-    xmin = -3
-    xmax = total_num_trials + 3
-    ymin = min(reached_y_negative + stalled_y_negative) - 3
-    ymax = max(reached_y_negative + stalled_y_positive) + 5
-    xhalfway = len(training_paps) + 1.5
-
-    ax.set_xlim(xmin, xmax)
-    ax.set_ylim(ymin, ymax)
-    ax.set_title("")
-
-    # Create the scatter plots
-    ax.scatter(reached_x, reached_y_positive, c='green', marker='o')
-    ax.scatter(stalled_x, stalled_y_positive, c='green', marker='x')
-    ax.scatter(reached_x, reached_y_negative, c='red', marker='o')
-    ax.scatter(stalled_x, stalled_y_negative, c='red', marker='x')
-
-    # Add lines
-    ax.plot((0, total_num_trials), (0, 0), "b--")     # line at reward == 0
-    ax.plot((xhalfway, xhalfway), (ymin, ymax), "b--")     # line at reward == 0
-
-    # Add textual information
-    ax.set_xlabel('Trial #')
-    ax.set_ylabel('Reward')
+# def plot_performances(training_paps, evaluation_paps):
+#     all_paps = training_paps + evaluation_paps
+#     total_num_trials = len(all_paps)
+#
+#     reached, stalled = separate_performances(all_paps)
+#
+#     reached_x = [trial for trial, pap in reached]
+#     reached_y_positive = [pap.positive_reward for _, pap in reached]
+#     reached_y_negative = [pap.negative_reward for _, pap in reached]
+#
+#     stalled_x = [trial for trial, pap in stalled]
+#     stalled_y_positive = [pap.positive_reward for _, pap in stalled]
+#     stalled_y_negative = [pap.negative_reward for _, pap in stalled]
+#
+#     fig, ax = plt.subplots(figsize=(10, 6))
+#
+#     xmin = -3
+#     xmax = total_num_trials + 3
+#     ymin = min(reached_y_negative + stalled_y_negative) - 3
+#     ymax = max(reached_y_positive + stalled_y_positive) + 5
+#     xhalfway = len(training_paps) + 1.5
+#
+#     ax.set_xlim(xmin, xmax)
+#     ax.set_ylim(ymin, ymax)
+#     ax.set_title("")
+#
+#     # Create the scatter plots
+#     ax.scatter(reached_x, reached_y_positive, c='green', marker='o')
+#     ax.scatter(stalled_x, stalled_y_positive, c='green', marker='x')
+#     ax.scatter(reached_x, reached_y_negative, c='red', marker='o')
+#     ax.scatter(stalled_x, stalled_y_negative, c='red', marker='x')
+#
+#     # Add lines
+#     ax.plot((0, total_num_trials), (0, 0), "k--")     # line at reward == 0
+#     ax.plot((xhalfway, xhalfway), (ymin, ymax), "k--")     # line at reward == 0
+#
+#     # Add textual information
+#     ax.set_xlabel('Trial #')
+#     ax.set_ylabel('Reward')
 
 if __name__ == '__main__':
-    t, e = evaluate_performance_helper(0.5, 0.8, exploiter, 2)
-    plot_performances(t, e)
+    gs = parameter_gridsearch()
+    result = next(gs)
+    result.plot()
+
